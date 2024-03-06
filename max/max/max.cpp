08185e94ba88765e4ccd22e759a404820937fed3
Ahmed anwer elrweny <146839312+ELRWENY@users.noreply.github.com>
2024-03-06 13:22:16 +0200

    Add files via upload

---
+++ max/max/max.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int x;
+	int n;
+	int max = 0;
+	cin >> n;
+	for (int i=0;i< n; i++) {
+		cin >> x;
+		if (x > max)
+			max = x;
+	}
+	cout << max;
+
+
+
+
+}
+
