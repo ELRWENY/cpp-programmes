746b70d9c7a2c7721e3d4af05c849df474a3337f
Ahmed anwer elrweny <146839312+ELRWENY@users.noreply.github.com>
2024-03-06 13:15:44 +0200

    Add files via upload

---
+++ WINTER SALE/WINTER SALE/WINTER SALE.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream> 
+using namespace std;
+int main(void) {
+
+    float discount;
+    float main_price;
+    float price_after_sale;
+    float value;
+    cin >> discount;
+    cin >> price_after_sale;
+    value = 100 - discount;
+    main_price = ((price_after_sale * 100) / value);
+    cout << main_price;
+    return 0;
+}
