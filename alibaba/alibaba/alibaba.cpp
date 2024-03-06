089427c358ae231df1c3c8c52f53e7b8d55ce510
Ahmed anwer elrweny <146839312+ELRWENY@users.noreply.github.com>
2024-03-06 13:20:30 +0200

    Add files via upload

---
+++ alibaba/alibaba/alibaba.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long x, y, z, d;
+    cin >> x >> y >> z >> d;
+
+    if (x + y * z == d)
+        cout << "YES";
+    else if (x + y - z == d)
+        cout << "YES";
+    else if (x - y * z == d)
+        cout << "YES";
+    else if (x - y + z == d)
+        cout << "YES";
+    else if (x * y - z == d)
+        cout << "YES";
+    else if (x * y + z == d)
+        cout << "YES";
+    else 
+        cout << "NO";
+}
