caabf497ff4d37f2adaba1884f6f86e819d93a45
Ahmed anwer elrweny <146839312+ELRWENY@users.noreply.github.com>
2024-03-09 17:28:31 +0200

    Add files via upload

---
+++ summation/summation/summation.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <array>
+#include <iomanip>
+using namespace std;
+int main()
+{//#pragma region summation task
+
+int  size;
+cin >> size;
+long long sum = 0;
+for (int i = 1; i <= size; i++) {
+	int numbers;
+	cin >> numbers;
+
+	sum = sum + numbers;
+
+}
+cout << abs(sum);
+}
+
