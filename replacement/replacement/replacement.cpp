c01f5c0dbf1fed6c53b86c275396a21fa28c8486
Ahmed anwer elrweny <146839312+ELRWENY@users.noreply.github.com>
2024-03-09 17:29:09 +0200

    Add files via upload

---
+++ replacement/replacement/replacement.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <array>
+#include <iomanip>
+using namespace std;
+int main()
+{//#pragma region replacement task
+	int size;
+	cin >> size;
+	int arr[size];
+	for (int i = 0; i < size; i++) {
+		cin >> arr[i];
+	}
+	int elemants;
+	cin >> elemants;
+
+	for (int i = 0; i < size; i++) {
+
+		if (arr[i] > 0) {
+
+			arr[i] = 1;
+
+		}
+		else if (arr[i] < 0) {
+			arr[i] = 2;
+
+		}
+
+	}
+	for (int i = 0; i < size; i++) {
+
+		cout << arr[i] << " ";
+	}
+
+}
