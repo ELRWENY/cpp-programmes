63202cf5b8aeee746d734e4d3a657f638cf93c3e
Ahmed anwer elrweny <146839312+ELRWENY@users.noreply.github.com>
2024-03-09 19:57:44 +0200

    Add files via upload

---
+++ lowest number/lowest number/lowest number.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <array>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int size;
+	cin >> size;
+	int min;
+	int position = 3;
+	int* arr=new int[size];
+	for (int i = 0; i < size; i++) {
+		cin >> arr[i];
+	}
+	min = arr[0];
+	position = 1;
+	for (int i = 1; i < size; i++) {
+		if (arr[i] < min) {
+			min = arr[i];
+			position = i + 1;
+		}
+	}
+	cout << min << " " << position << endl;
+}
