f756984bc6d67cf84316768b3ae9c873696cca6c
Ahmed anwer elrweny <146839312+ELRWENY@users.noreply.github.com>
2024-03-09 17:28:53 +0200

    Add files via upload

---
+++ searching/searching/searching.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int size;
+	cin >> size;
+	int check = 0;
+	int arr[size];
+	for (int i = 0; i < size; i++) {
+		cin >> arr[i];
+	}
+	int searcher;
+	cin >> searcher;
+	for (int i = 0; i < size; i++) {
+		if (arr[i] == searcher) {
+			cout << i << endl;
+			check = 1;
+			break;
+		}
+	}
+	if (check == 0) {
+		cout << -1 << endl;
+	}
+}
