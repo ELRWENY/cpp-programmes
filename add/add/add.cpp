cb0138f3ce6776b122655dac74a84537d97f8ad4
Ahmed anwer elrweny <146839312+ELRWENY@users.noreply.github.com>
2024-03-16 09:58:24 +0200

    Add files via upload

---
+++ add/add/add.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void  sum(int num1, int num2)
+
+{
+    cin >> num1;
+    cin >> num2;
+    cout << (num1 + num2);
+}
+int main()
+{
+    sum(1, 2);
+    return 0;
+}
